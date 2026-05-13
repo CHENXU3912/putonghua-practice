a4f74932418fb848a8b7cda9fe30df92b4f29be6
阿琛 <achen@putonghua.app>
2026-05-14 00:03:02 +0800

    switch to static export

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // 静态导出（可选：如果需要纯静态部署到 Cloudflare Pages 等）
-  // output: 'export',
+  output: 'export',
 };
 
 export default nextConfig;
